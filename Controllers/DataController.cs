65ddd4e3d2091f158de4d4e754c8aea39696d785
Kiran Sawant <sawantk389@gmail.com>
2023-08-03 02:35:25 +0530

    Add project files.

---
+++ Controllers/DataController.cs
@@ -0,0 +1,22 @@
+﻿using Proj1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proj1.Controllers
+{
+    public class DataController : Controller
+    {
+        public JsonResult UserLogin(LoginData d)
+        {
+            using (Connection dc = new Connection())
+            {
+                var user = dc.Users.Where(a => a.UserName.Equals(d.UserName) && a.Password.Equals(d.Password)).FirstOrDefault();
+                return new JsonResult { Data = user, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+    }
+}
